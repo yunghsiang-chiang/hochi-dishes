7d5345d481035d7714cf53bbec3b3a48173d668d
yunghsiang-chiang <yunghsiang.chiang@hochi.org.tw>
2024-11-05 14:54:40 +0800

    2024/11/5 14:54

--- food/Site.Master.designer.cs
+++ food/Site.Master.designer.cs
@@ -24,15 +24,6 @@ namespace food
         protected global::System.Web.UI.HtmlControls.HtmlAnchor menu_infor;
 
         /// <summary>
-        /// login_inout 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor login_inout;
-
-        /// <summary>
         /// MainContent 控制項。
         /// </summary>
         /// <remarks>
