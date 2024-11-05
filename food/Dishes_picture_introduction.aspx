a159be35ea3c55bcc2801c6fe82f7bc1af49bbd2
yunghsiang-chiang <yunghsiang.chiang@hochi.org.tw>
2024-11-05 14:40:03 +0800

    2024/11/5 14:39

--- food/Dishes_picture_introduction.aspx
+++ food/Dishes_picture_introduction.aspx
@@ -1,8 +1,20 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Dishes_picture_introduction.aspx.cs" Inherits="food.Dishes_picture_introduction" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-    <script src="Scripts/RootPage/Dishes_picture_introduction.js"></script>
-    <div class="parent-container" id="dishes_menu">
-
+    <div class="container mt-5">
+        <h2 class="text-center">主廚食譜數量</h2>
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-8">
+                <!-- Chart Canvas -->
+                <canvas id="chefRecipeChart"></canvas>
+            </div>
+        </div>
     </div>
+
+    <!-- 引入 Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <!-- 引入 jQuery -->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <!-- 引入自訂 JS 檔案 -->
+    <script src="Scripts/RootPage/Dishes_picture_introduction.js"></script>
 </asp:Content>
--- food/Scripts/RootPage/Dishes_picture_introduction.js
+++ food/Scripts/RootPage/Dishes_picture_introduction.js
@@ -1,4 +1,56 @@
-﻿// event at page loaded
-$(document).ready(function () {
-    
-})
+﻿$(document).ready(function () {
+    // API URL
+    const apiUrl = "http://internal.hochi.org.tw:8082/api/Chefs/chefs/recipeCounts";
+
+    // 使用 AJAX 取得資料
+    $.getJSON(apiUrl, function (data) {
+        // 過濾資料，提取 chefName 和 recipeCount
+        const chefNames = data.$values.map(item => item.chefName);
+        const recipeCounts = data.$values.map(item => item.recipeCount);
+
+        // 建立 Chart.js 圖表
+        const ctx = document.getElementById('chefRecipeChart').getContext('2d');
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: chefNames,
+                datasets: [{
+                    label: 'Recipe Count',
+                    data: recipeCounts,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        title: {
+                            display: true,
+                            text: 'Chef Name'
+                        }
+                    },
+                    y: {
+                        beginAtZero: true,
+                        title: {
+                            display: true,
+                            text: 'Recipe Count'
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        position: 'top'
+                    },
+                    tooltip: {
+                        enabled: true
+                    }
+                }
+            }
+        });
+    }).fail(function () {
+        alert("Failed to load data from API.");
+    });
+});
